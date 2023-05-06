9b7793f5e4bf2f479a5f63c0cdc2b7e7e166be30
jesusbeb <jesusbelb@gmail.com>
2023-05-06 14:39:58 -0600

    Random() parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice2.png';
+  
+  numero1 = 1;
+  numero2 = 2;
+
+
+
+  tirarDados(): void{
+    //Math.random() regresa un numero aleatorio en 0 y 1
+    //Se multiplica *5 y al valor final se le suma 1 para que no nos de valores de 0 ni de 7
+    //Math.round redondea al numero entero mas cercano
+    this.numero1 = Math.round(Math.random()*5)+1;
+    this.numero2 = Math.round(Math.random()*5)+1;
+
+    
+  }
 }
